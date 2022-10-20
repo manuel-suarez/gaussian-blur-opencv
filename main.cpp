a649bed247354c7e05db43f059f68decd2ae894a
Arturo Suarez <manuel.suarez@cimat.mx>
2022-10-20 11:38:23 -0500

    Initialization

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by masuarez on 20/10/22.
 //
+#include <opencv4/opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Read image file
+    Mat image = imread("")
+}
